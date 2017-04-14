8636204e7343cf5cb58d743303a6c71ba188276c
Thomas Frengler <thomasfrengler@gmail.com>
2017-04-14 16:42:02 +0200

    Javascript restructuring
    The JS was actually okay; scoped, and nicely formatted and robust but
    being a stickler for well organized code I decided to redo it.
    Functionally it's the same. It's purely the structure. Now it's better,
    makes more sense and easier to expand upon going forward.

--- Application/Assets/main.js
+++ Application/Assets/main.js
@@ -1,267 +1,442 @@
 "use strict";
 
-var RecipeDB = {
-	MainContentContainerID: "MainContent"
+/* Main container, a namespace for all our functionality */
+var RecipeDB = {};
+
+/* MAIN.CFM */
+
+RecipeDB.Main = {
+	DOM: {
+		Pointers: {},
+		ElementData: {}
+	},
+	Methods: {}
 };
 
-/* RECIPE VIEW */
-
-RecipeDB.Recipe = {
-	"Description-Body": {},
-	"Ingredients-Body": {},
-	"Instructions-Body": {},
-	"Comments-Body": {},
-	"Status-Body": {}
-};
-
-RecipeDB.Recipe.OpenCloseSection = function(Caller) {
-	var HeaderSection = Caller;
-	var BodySection = Caller.nextElementSibling;
-
-	if ( $(BodySection).css("display") == "block" ) {
-		$(BodySection).animate({height: 0}, 500, function() {
-			$(BodySection).hide();
-			$(HeaderSection).css({display: "block", "text-align": "center"});
-		});
-	}
-	else {
-		$(BodySection).show();
-		$(BodySection).animate({height: RecipeDB.Recipe[BodySection.id].Height}, 500, function() {
-			$(BodySection).css("height", "");
-			$(HeaderSection).css({display: "inline-block", "text-align": ""});
-		});
+RecipeDB.Main.DOM.ElementData = {
+	MainContentContainer: {
+		ID: "MainContent"
 	}
 };
 
-RecipeDB.Recipe.SetSectionDimensions = function() {
-	RecipeDB.Recipe["Description-Body"].Height = parseFloat( $("#Description-Body").css("height") );
-	RecipeDB.Recipe["Ingredients-Body"].Height = parseFloat( $("#Ingredients-Body").css("height") );
-	RecipeDB.Recipe["Instructions-Body"].Height = parseFloat( $("#Instructions-Body").css("height") );
-	RecipeDB.Recipe["Comments-Body"].Height = parseFloat( $("#Comments-Body").css("height") );
-	RecipeDB.Recipe["Status-Body"].Height = parseFloat( $("#Status-Body").css("height") );
+RecipeDB.Main.init = function() {
+
 };
 
-RecipeDB.Recipe.SetPictureEditDimensions = function() {
-	$('#Recipe-Picture-Edit').css('height', $('#Recipe-Picture').css('height'));
-	$('#Recipe-Picture-Edit').css('width', $('#Recipe-Picture').css('width'));
-};
-
-RecipeDB.Recipe.init = function() {
-	this.SetSectionDimensions();
-
-	$("[name='Recipe-Header']").click(function() {
-		RecipeDB.Recipe.OpenCloseSection(this)
-	});
-
-	$("#Recipe-Picture").load(function() {
-		RecipeDB.Recipe.SetPictureEditDimensions();
-	});
-};
-
-RecipeDB.Recipe.onResize = function() {
-	RecipeDB.Recipe.SetSectionDimensions();
-	RecipeDB.Recipe.SetPictureEditDimensions();
-	RecipeDB.Menu.SetMenuDimensions();
-};
-
-/* MAIN MENU */
-
-RecipeDB.Menu = {
-	MenuID :"side-menu",
-	MenuOptionsID :"menu-options",
-	CloseButtonID: "Close-Menu-Button",
-	OpenButtonID: "Open-Menu-Button",
-	LogoutOptionID: "Logout",
-	UserSettingsOptionID: "UserSettings"
-};
-
-RecipeDB.Menu.SetMenuDimensions = function() {
-	this.MenuWidth = parseFloat( $("#" + this.MenuID).css("width") ) + 1;
-};
-
-RecipeDB.Menu.onResize = function() {
-	this.SetMenuDimensions();
-};
-
-RecipeDB.Menu.Show = function() {
-	$("#" + this.MenuID).animate({width: this.MenuWidth}, 500);
-	$("#" + this.MenuOptionsID).show(500);
-};
-
-RecipeDB.Menu.Hide = function() {
-	$("#" + this.MenuOptionsID).hide(200);
-	$("#" + this.MenuID).animate({width: 0}, 500);
-};
-
-RecipeDB.Menu.init = function() {
-	this.SetMenuDimensions();
-
-	$("#" + this.CloseButtonID).click(function() {
-		RecipeDB.Menu.Hide();
-	});
-
-	$("#" + this.OpenButtonID).click(function() {
-		RecipeDB.Menu.Show();
-	});
-
-	$("#" + this.LogoutOptionID).click(function() {
-		RecipeDB.Menu.Logout();
-	});
-
-	$("#" + this.LogoutOptionID).click(function() {
-		RecipeDB.Menu.Logout();
-	});
-
-	$("#" + this.UserSettingsOptionID).click(function() {
-		RecipeDB.Menu.UserSettings();
-	});
-
-	this.Hide();
-};
-
-RecipeDB.Menu.Logout = function() {
-	$.ajax({
-		type: "post",
-		url: "../../AuthenticationManager.cfc",
-		data: {
-			method: "gracefulLogout",
-			Reason: 2
-		},
-		dataType: "json",
-
-		success: function(ResponseData) {
-			RecipeDB.Menu.OnLogoutComplete(ResponseData);
-		}
-	});
-};
-
-RecipeDB.Menu.UserSettings = function() {
-	$.ajax({
-		type: "post",
-		url: "../Controllers/UserController.cfc",
-		data: {
-			method: "getUserSettingsView"
-		},
-		dataType: "html",
-
-		success: function(ResponseData) {
-			RecipeDB.Menu.OnGetUserSettingsComplete(ResponseData);
-		}
-	});
-};
-
-RecipeDB.Menu.OnGetUserSettingsComplete = function(AjaxResponse) {
-	$("#" + RecipeDB.MainContentContainerID).html(AjaxResponse);
-};
-
-RecipeDB.Menu.OnLogoutComplete = function(AjaxResponse) {	
-	window.location.replace("../../Login.cfm?Reason=2");
-};
-
-RecipeDB.Menu.OnLogoutError = function(AjaxResponse) {
-	// $('#' + this.MessageBoxID).html("Internal error. Please try again or contact the administrator");
-	console.warn(AjaxResponse);
-};
-
-/* LOGIN PAGE */
+/* LOGIN.CFM */
 
 RecipeDB.LoginPage = {
-	MessageBoxID: "Login-MessageBox",
-	LoginButtonID: "Login-Button",
-	PasswordID: "Password",
-	UsernameID: "Username"
+	DOM: {
+		Pointers: {},
+		ElementData: {}
+	},
+	Methods: {}
 };
 
-RecipeDB.LoginPage.attemptLogin = function() {
-	$('#' + RecipeDB.LoginPage.MessageBoxID).hide();
+RecipeDB.LoginPage.DOM.ElementData = {
 
-	var Password = $('#' + this.PasswordID).val();
-	var Username = $('#' + this.UsernameID).val();
+	MessageBox: {
+		ID: "Login-MessageBox"
+	},
 
-	if ( Username.length === 0 ) {
-		$('#' + RecipeDB.LoginPage.MessageBoxID).html("Please enter a username");
-		$('#' + RecipeDB.LoginPage.MessageBoxID).fadeIn(1000);
-		return false;
-	};
+	LoginButton: {
+		ID: "Login-Button"
+	},
 
-	$.ajax({
-		type: "post",
-		url: "AuthenticationManager.cfc",
-		data: {
-			method: "attemptLogin",
-			password: Password,
-			username: Username
-		},
-		dataType: "json",
+	Password: {
+		ID: "Password"
+	},
 
-		success: function(ResponseData) {
-			RecipeDB.LoginPage.OnLoginComplete(ResponseData);
-		},
-		error: function(ResponseData) {
-			RecipeDB.LoginPage.OnLoginError(ResponseData);
-		},
-		beforeSend: function() {
-			$('#' + RecipeDB.LoginPage.LoginButtonID).prop("disabled", true);
-			$('#' + RecipeDB.LoginPage.LoginButtonID).val("OK");
-			$('#' + RecipeDB.LoginPage.LoginButtonID).addClass("ajax-loading");
-		
-		},
-		complete: function() {
-			$('#' + RecipeDB.LoginPage.LoginButtonID).prop("disabled", false);
-			$('#' + RecipeDB.LoginPage.LoginButtonID).removeClass("ajax-loading");
-			$('#' + RecipeDB.LoginPage.LoginButtonID).val("OK");
-		}
-	});
-};
-
-RecipeDB.LoginPage.OnLoginComplete = function(AjaxResponse) {
-
-	$('#' + this.MessageBoxID).hide();
-
-	if (AjaxResponse.Result === false) {
-		if (AjaxResponse.Code > 0) {
-			$('#' + this.MessageBoxID).html("Your username and/or password is wrong");
-		}
-
-		$('#' + this.MessageBoxID).fadeIn(1000);
-		return false;
-	};
-
-	window.location.replace("Application/Views/Main.cfm");
-};
-
-RecipeDB.LoginPage.OnLoginError = function(AjaxResponse) {
-	$('#' + this.MessageBoxID).html("Internal error. Please try again or contact the administrator");
-	console.warn(AjaxResponse);
+	Username: {
+		ID: "Username"
+	}
 };
 
 RecipeDB.LoginPage.init = function() {
+
 	$('#Login-Button').click(function() {
-		RecipeDB.LoginPage.attemptLogin();
+		RecipeDB.LoginPage.Methods.attemptLogin();
 	});
+
 };
 
-/* USER SETTINGS */
+RecipeDB.LoginPage.Methods = {
+
+	attemptLogin: function() {
+		$('#' + RecipeDB.LoginPage.DOM.ElementData.MessageBox.ID).hide();
+
+		var Password = $('#' + RecipeDB.LoginPage.DOM.ElementData.Password.ID).val();
+		var Username = $('#' + RecipeDB.LoginPage.DOM.ElementData.Username.ID).val();
+
+		if ( Username.length === 0 || Username === " " ) {
+			$('#' + RecipeDB.LoginPage.DOM.ElementData.MessageBox.ID).html("Please enter a username");
+			$('#' + RecipeDB.LoginPage.DOM.ElementData.MessageBox.ID).fadeIn(1000);
+			return false;
+		};
+
+		$.ajax({
+			type: "post",
+			url: "AuthenticationManager.cfc",
+			data: {
+				method: "attemptLogin",
+				password: String(Password),
+				username: String(Username)
+			},
+			dataType: "json",
+
+			success: function(ResponseData) {
+				RecipeDB.LoginPage.Methods.onLoginComplete(ResponseData);
+			},
+			error: function() {
+				RecipeDB.LoginPage.Methods.onLoginError(arguments);
+			},
+			beforeSend: function() {
+				$('#' + RecipeDB.LoginPage.DOM.ElementData.LoginButton.ID).prop("disabled", true);
+				$('#' + RecipeDB.LoginPage.DOM.ElementData.LoginButton.ID).val("OK");
+				$('#' + RecipeDB.LoginPage.DOM.ElementData.LoginButton.ID).addClass("ajax-loading");
+			
+			},
+			complete: function() {
+				$('#' + RecipeDB.LoginPage.DOM.ElementData.LoginButton.ID).prop("disabled", false);
+				$('#' + RecipeDB.LoginPage.DOM.ElementData.LoginButton.ID).removeClass("ajax-loading");
+				$('#' + RecipeDB.LoginPage.DOM.ElementData.LoginButton.ID).val("OK");
+			}
+		});
+	},
+
+	onLoginComplete: function(AjaxResponse) {
+
+		$('#' + RecipeDB.LoginPage.DOM.ElementData.MessageBox.ID).hide();
+
+		if (AjaxResponse.Result === false) {
+			if (AjaxResponse.Code > 0) {
+				$('#' + RecipeDB.LoginPage.DOM.ElementData.MessageBox.ID).html("Your username and/or password is wrong");
+			}
+
+			$('#' + RecipeDB.LoginPage.DOM.ElementData.MessageBox.ID).fadeIn(1000);
+			return false;
+		};
+
+		window.location.replace("Application/Views/Main.cfm");
+	},
+
+	onLoginError: function(AjaxResponse) {
+		$('#' + RecipeDB.LoginPage.DOM.ElementData.MessageBox.ID).html("Internal error. Please try again or contact the administrator");
+		console.warn(AjaxResponse);
+	}
+};
+
+/* USER SETTINGS.CFM */
 
 RecipeDB.UserSettings = {
-	DisplayNameBoxID: "DisplayName",
-	UsernameBoxID: "Username",
-	PasswordBoxID: "SecretKey",
-	AccountCreationDateBoxID: "AccountCreationDate",
-	LoginCountBoxID: "TimesLoggedIn",
-	BrowserLastUsedBoxID: "BrowserLastUsed"
+	DOM: {
+		Pointers: {},
+		ElementData: {}
+	},
+	Methods: {}
+};
+
+RecipeDB.UserSettings.DOM.ElementData = {
+
+	DisplayNameBox: {
+		ID: "DisplayName"
+	},
+
+	UsernameBox: {
+		ID: "Username"
+	},
+
+	PasswordBox: {
+		ID: "SecretKey"
+	},
+
+	AccountCreationDateBox: {
+		ID: "AccountCreationDate"
+	},
+
+	LoginCountBox: {
+		ID: "TimesLoggedIn"
+	},
+
+	BrowserLastUsedBox: {
+		ID: "BrowserLastUsed"
+	}
 };
 
 RecipeDB.UserSettings.init = function() {
 
 };
 
-/* MAIN */
+/* MENU.CFM */
 
-RecipeDB.Main = {};
+RecipeDB.Menu = {
+	DOM: {
+		Pointers: {},
+		ElementData: {}
+	},
+	Methods: {}
+};
 
-RecipeDB.Main.init = function() {
+RecipeDB.Menu.DOM.ElementData = {
+	Menu: {
+		ID :"side-menu",
+		Width: 0
+	},
 
+	MenuOptions: {
+		ID :"menu-options"
+	},
+
+	CloseButton: {
+		ID: "Close-Menu-Button"
+	},
+
+	OpenButton: {
+		ID: "Open-Menu-Button"
+	},
+
+	LogoutOption: {
+		ID: "Logout"
+	},
+
+	UserSettingsOption: {
+		ID: "UserSettings"
+	}
+}
+
+RecipeDB.Menu.init = function() {
+	RecipeDB.Menu.Methods.setMenuDimensions();
+	
+	$(document).resize(function() {
+		RecipeDB.Menu.Methods.onResize();
+	});
+
+	$("#" + RecipeDB.Menu.DOM.ElementData.CloseButton.ID).click(function() {
+		RecipeDB.Menu.Methods.hide();
+	});
+
+	$("#" + RecipeDB.Menu.DOM.ElementData.OpenButton.ID).click(function() {
+		RecipeDB.Menu.Methods.show();
+	});
+
+	$("#" + RecipeDB.Menu.DOM.ElementData.LogoutOption.ID).click(function() {
+		RecipeDB.Menu.Methods.logout();
+	});
+
+	$("#" + RecipeDB.Menu.DOM.ElementData.UserSettingsOption.ID).click(function() {
+		RecipeDB.Menu.Methods.getUserSettings();
+	});
+
+	RecipeDB.Menu.Methods.hide();
+};
+
+RecipeDB.Menu.Methods = {
+
+	logout: function() {
+		$.ajax({
+			type: "post",
+			url: "../../AuthenticationManager.cfc",
+			data: {
+				method: "gracefulLogout",
+				Reason: 2
+			},
+			dataType: "json",
+
+			success: function(ResponseData) {
+				RecipeDB.Menu.Methods.onLogoutComplete(ResponseData);
+			}
+		});
+	},
+
+	getUserSettings: function() {
+		$.ajax({
+			type: "post",
+			url: "../Controllers/UserController.cfc",
+			data: {
+				method: "getUserSettingsView"
+			},
+			dataType: "html",
+
+			success: function(ResponseData) {
+				RecipeDB.Menu.Methods.onGetUserSettingsComplete(ResponseData);
+			}
+		});
+	},
+
+	onGetUserSettingsComplete: function(AjaxResponse) {
+		$("#" + RecipeDB.Main.DOM.ElementData.MainContentContainer.ID).html( String(AjaxResponse) );
+	},
+
+	onLogoutComplete: function(AjaxResponse) {
+		window.location.replace("../../Login.cfm?Reason=2");
+	},
+
+	hide: function() {
+		$("#" + RecipeDB.Menu.DOM.ElementData.MenuOptions.ID).hide(200);
+		$("#" + RecipeDB.Menu.DOM.ElementData.Menu.ID).animate({width: 0}, 500);
+	},
+
+	show: function() {
+		$("#" + RecipeDB.Menu.DOM.ElementData.Menu.ID).animate({width: RecipeDB.Menu.DOM.ElementData.Menu.Width}, 500);
+		$("#" + RecipeDB.Menu.DOM.ElementData.MenuOptions.ID).show(500);
+	},
+
+	onResize: function() {
+		RecipeDB.Menu.Methods.setMenuDimensions();
+	},
+
+	setMenuDimensions: function() {
+		RecipeDB.Menu.DOM.ElementData.Menu.Width = parseFloat( $("#" + RecipeDB.Menu.DOM.ElementData.Menu.ID).css("width") ) + 1;
+	}
+};
+
+/* RECIPE.CFM */
+
+RecipeDB.Recipe = {
+	DOM: {
+		Pointers: {},
+		ElementData: {}
+	},
+	Methods: {}
+};
+
+RecipeDB.Recipe.DOM.ElementData = {
+
+	SectionHeaders: {
+		Name: "Recipe-Header"
+	},
+
+	DescriptionBody: {
+		ID: "Description-Body",
+		Height: 0
+	},
+
+	IngredientsBody: {
+		ID: "Ingredients-Body",
+		Height: 0
+	},
+	
+	InstructionsBody: {
+		ID: "Instructions-Body",
+		Height: 0
+	},
+	
+	CommentsBody: {
+		ID: "Comments-Body",
+		Height: 0
+	},
+
+	StatusBody: {
+		ID: "Status-Body",
+		Height: 0
+	},
+
+	Picture: {
+		ID: "Recipe-Picture"
+	},
+
+	PictureEdit: {
+		ID: "Recipe-Picture-Edit"
+	}
+};
+
+RecipeDB.Recipe.init = function() {
+	RecipeDB.Recipe.Methods.setSectionDimensions();
+
+	$( trim(" [name=' " + RecipeDB.Recipe.DOM.ElementData.SectionHeaders.Name + " '] ") ).click(function() {
+		RecipeDB.Recipe.Methods.openCloseSection(this)
+	});
+
+	$("#" + RecipeDB.Recipe.DOM.ElementData.Picture).load(function() {
+		RecipeDB.Recipe.Methods.setPictureEditDimensions();
+	});
+
+	$(document).resize( function() {
+		RecipeDB.Recipe.Methods.onResize();
+	});
+};
+
+RecipeDB.Recipe.Methods = {
+
+	onResize: function() {
+		RecipeDB.Recipe.Methods.setSectionDimensions();
+		RecipeDB.Recipe.Methods.setPictureEditDimensions();
+	},
+
+	setSectionDimensions: function() {
+
+		RecipeDB.Recipe.DOM.ElementData.DescriptionBody.Height = parseFloat( 
+			$("#" + RecipeDB.Recipe.DOM.ElementData.DescriptionBody.ID).css("height") 
+		);
+
+		RecipeDB.Recipe.DOM.ElementData.IngredientsBody.Height = parseFloat( 
+			$("#" + RecipeDB.Recipe.DOM.ElementData.IngredientsBody.ID).css("height") 
+		);
+
+		RecipeDB.Recipe.DOM.ElementData.InstructionsBody.Height = parseFloat( 
+			$("#" + RecipeDB.Recipe.DOM.ElementData.InstructionsBody.ID).css("height") 
+		);
+
+		RecipeDB.Recipe.DOM.ElementData.CommentsBody.Height = parseFloat( 
+			$("#" + RecipeDB.Recipe.DOM.ElementData.CommentsBody.ID).css("height") 
+		);
+
+		RecipeDB.Recipe.DOM.ElementData.StatusBody.Height = parseFloat( 
+			$("#" + RecipeDB.Recipe.DOM.ElementData.StatusBody.ID).css("height") 
+		);
+
+	},
+
+	setPictureEditDimensions: function() {
+
+		$('#' + RecipeDB.Recipe.DOM.ElementData.PictureEdit.ID).css(
+			'height', 
+			$('#' + RecipeDB.Recipe.DOM.ElementData.Picture.ID).css('height')
+		);
+
+		$('#' + RecipeDB.Recipe.DOM.ElementData.PictureEdit.ID).css(
+			'width', 
+			$('#' + RecipeDB.Recipe.DOM.ElementData.Picture.ID).css('width')
+		);
+
+	},
+
+	openCloseSection: function(Caller) {
+		
+		var HeaderSection = Caller;
+		var BodySection = Caller.nextElementSibling;
+		var DesiredHeight = 0;
+		var Key = "";
+		var CurrentElementData = {};
+
+		for (Key in RecipeDB.Recipe.DOM.ElementData) {
+			CurrentElementData = RecipeDB.Recipe.DOM.ElementData[Key];
+			
+			if ( CurrentElementData.ID === BodySection.id) {
+				DesiredHeight = CurrentElementData.Height;
+				break;
+			}
+		};
+
+		if (typeof DesiredHeight === 'undefined' ) {
+			console.warn("DesiredHeight is undefined :(");
+			return false;
+		};
+
+		if ( $(BodySection).css("display") == "block" ) {
+			$(BodySection).animate({height: 0}, 500, function() {
+				$(BodySection).hide();
+				$(HeaderSection).css({display: "block", "text-align": "center"});
+			});
+		}
+		else {
+			$(BodySection).show();
+			$(BodySection).animate({height: DesiredHeight}, 500, function() {
+				$(BodySection).css("height", "");
+				$(HeaderSection).css({display: "inline-block", "text-align": ""});
+			});
+		}
+	}
 };
 
 /* UTILITIES */
